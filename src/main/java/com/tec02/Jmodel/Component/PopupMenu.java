df9c2cf0606c125fe453bd31c05de5b328e31c04
SQT <samtruyen60@mail.com>
2024-03-16 16:14:19 +0700

    the addMenu function cho popmenu

--- src/main/java/com/tec02/Jmodel/Component/PopupMenu.java
+++ src/main/java/com/tec02/Jmodel/Component/PopupMenu.java
@@ -7,6 +7,8 @@ package com.tec02.Jmodel.Component;
 import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JMenuItem;
@@ -17,35 +19,51 @@ import javax.swing.JPopupMenu;
  * @author Administrator
  */
 public class PopupMenu {
-    
+
     private final JPopupMenu popupMenu;
+    private final List<JMenuItem> menuItems;
 
     public PopupMenu() {
         this.popupMenu = new JPopupMenu();
+        this.menuItems = new ArrayList<>();
     }
-    
-    public void addItemMenu(String itemName, ActionListener actionListener){
+
+    public void addItemMenu(String itemName, ActionListener actionListener) {
         JMenuItem itemMenu = new JMenuItem(itemName);
         itemMenu.addActionListener(actionListener);
+        addItemMenu(itemMenu);
+    }
+
+    public void addItemMenu(JMenuItem itemMenu) {
+        this.menuItems.add(itemMenu);
         this.popupMenu.add(itemMenu);
     }
-    
-    public void addItemMenu(String itemName, ActionListener actionListener, Icon icon){
+
+    public void addMenu(PopupMenu menu) {
+        if (menu == null) {
+            return;
+        }
+        for (JMenuItem menuItem : menu.menuItems) {
+            addItemMenu(menuItem);
+        }
+    }
+
+    public void addItemMenu(String itemName, ActionListener actionListener, Icon icon) {
         JMenuItem itemMenu = new JMenuItem(itemName);
         itemMenu.addActionListener(actionListener);
         itemMenu.setIcon(icon);
-        this.popupMenu.add(itemMenu);
+        addItemMenu(itemMenu);
     }
-    
-    public void addItemMenu(String itemName, ActionListener actionListener, String iconPath){
-        addItemMenu(itemName, actionListener, new ImageIcon(itemName));
+
+    public void addItemMenu(String itemName, ActionListener actionListener, String iconPath) {
+        addItemMenu(itemName, actionListener, new ImageIcon(iconPath));
     }
-    
-    public void show(Component component ,int x, int y){
+
+    public void show(Component component, int x, int y) {
         this.popupMenu.show(component, x, y);
     }
-    
-    public void show(MouseEvent e){
+
+    public void show(MouseEvent e) {
         show(e.getComponent(), e.getX(), e.getY());
     }
 
